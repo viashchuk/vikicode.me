8bd8a785d6a9c5825ce195795007bde762fd9fc4
viashchuk <victoriya.dev@icloud.com>
2023-10-29 12:29:22 +0100

    Fix

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 import './style.css'
+
+const style_console_msg = 'background-color: #f3e8ff; color: #6b21a8; border-radius: 4px; border: 1px solid #c084fc; font-size: 14px; padding: 2px 10px;'
+
+console.log("%cThank you for visiting my site. You can check out the source on the git", style_console_msg)
